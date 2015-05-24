b52aa60fb1d70d9319f203d1e2a6fc0b1912d6f7
ken <ken@localhost.localdomain>
2015-05-24 15:48:03 +0800

    E playtime copy price to line_items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,7 +29,7 @@ class LineItemsController < ApplicationController
   def create
 
     product = Product.find(params[:product_id]) 
-    @line_item = @cart.add_product(product.id)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
