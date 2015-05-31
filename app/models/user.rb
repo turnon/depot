0bb1b9c80f02ac207b294a80bb9ae4b09d74e786
ken <ken@localhost.localdomain>
2015-05-31 17:09:07 +0800

    I4 : check user count before delete user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  before_destroy :ensure_an_admin_remains
+  
+  private
+    def ensure_an_admin_remains
+      if User.count < 2
+        raise "Can't delete last user"
+      end
+    end
 end
